da516ee0bc94b18bab24bb70e07cd3887303b618
Natalio Sacerdote <natalio.sacerdote@edataconsulting.de>
2020-09-30 15:07:15 +0100

    Lists lesson

---
+++ lists-cond-01-starting-setup/app.js
@@ -0,0 +1,18 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      goal: '',
+      goals: []
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.goal);
+    },
+    removeGoal(index) {
+      this.goals.splice(index, 1);
+    }
+  }
+});
+
+app.mount('#user-goals');
